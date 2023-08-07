172eaf4eca303d4d1f2a151bd3139bd11df37304
Mattr7745 <140997018+Mattr7745@users.noreply.github.com>
2023-08-07 04:00:57 +0000

    changed price of cable feet from .87 to .97

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ print("Welcome to Matt's Fiber Optic Cable Outlet")
 company_name = input("Please enter your company name:")
 cable_feet = input("Please enter how many feet of fiber optic cable you wish to purchase:")
 cable_feet = int(cable_feet)
-total_cost = cable_feet * .87 #calculates feet of cable required by the cost of $.87
+total_cost = cable_feet * .97 #calculates feet of cable required by the cost of $.97
 total_cost = '${:,.2f}'.format(total_cost) #converts total_cost to dollar format
 print(f"{company_name}, the total cost for the installation of {cable_feet} feet of cable is: {total_cost}")
 input("Press return to exit ") #stops the program from closing immediately after displaying the output and allows the user to manually exit
